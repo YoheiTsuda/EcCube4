1b2f644141fb10909fc8d4ddc11cc658e0cfb45a
津田 陽平 <yohei.tsuda@water.local>
2020-03-16 19:13:27 +0900

    csv

--- src/Eccube/Controller/Admin/Content/SqloutputController.php
+++ src/Eccube/Controller/Admin/Content/SqloutputController.php
@@ -129,7 +129,8 @@ class SqloutputController extends AbstractController
      * @return StreamedResponse
      * @Route("/%eccube_admin_route%/content/sqloutput/{id}", requirements={"id" = "\d+"}, name="admin_content_sqloutput_export")
      */
-    public function exportCsv(Request $request)
+    public function exportCsv()
+    // public function exportCsv(Request $request)
     {
 // ★　通常のCSV出力ボタン
         // タイムアウトを無効にする.
